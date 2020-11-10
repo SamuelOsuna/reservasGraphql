82e7a2cba084a8dce2f215b4c164384e58dada70
SamuelOsuna <samuelosunaalcaide@gmail.com>
2020-11-10 18:01:55 +0100

    first commit

---
+++ src/main/java/com/tiagoamp/demographql/GraphqlSpringBootApp.java
@@ -0,0 +1,13 @@
+package com.tiagoamp.demographql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringBootApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlSpringBootApp.class, args);
+	}
+	
+}
